7cb1b062c1c68666e92e9d53149b0d062c739b09
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-02-16 20:46:52 +0330

    all files added , project started

---
+++ app.js
@@ -0,0 +1,20 @@
+// let users = ["ali", "mmd", "asghar", "mmd 2 ", "mmd 3 ", "mmd 4 "];
+// let containerElem = document.querySelector(".container");
+// let container2Elem = document.querySelector(".container2");
+// let i = 0;
+
+// users.forEach(function (user) {
+//   i++;
+//   if (i > 3) {
+//     secondeRowCreate();
+//   } else {
+//     containerElem.insertAdjacentHTML(
+//       "beforeend",
+//       '<div class="box">' + user + "</div> "
+//     );
+//   }
+// });
+
+// function secondeRowCreate() {
+//   container2Elem.insertAdjacentHTML("beforeend", '<div class="box"></div>');
+// }
