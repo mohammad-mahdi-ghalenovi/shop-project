cc6fc411cf68eec13c6786e9a05ce1ff5758bac5
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-03-02 14:53:58 +0330

    slider bugs fixed , gonna change css

--- Js/product.js
+++ Js/product.js
@@ -1,10 +1,25 @@
 let products = [
-  { id: 1, slides: ["1", "2", "3"] },
-  { id: 2, slides: ["4", "5", "6"] },
+  {
+    id: 1,
+    slides: [
+      "Imgs/wallpaperflare.com_wallpaper_15.jpg",
+      "Imgs/wallpaperflare.com_wallpaper_2.jpg",
+      "Imgs/wallpaperflare.com_wallpaper_5.jpg",
+    ],
+  },
+  {
+    id: 2,
+    slides: [
+      "Imgs/wallpaperflare.com_wallpaper_15.jpg",
+      "Imgs/wallpaperflare.com_wallpaper_5.jpg",
+      "Imgs/wallpaperflare.com_wallpaper_2.jpg",
+    ],
+  },
   { id: 3, slides: ["7", "8", "9"] },
 ];
 let productSlider = document.querySelector(".product-slider");
 let productSlides = document.querySelector(".product-slides");
+let i = 0;
 // URL PARAM
 let locationSearch = location.search;
 let SearchUrlParam = new URLSearchParams(locationSearch);
@@ -20,20 +35,32 @@ function findTargetObj(searchID) {
 
   targetSlider = mainProduct;
 
-  createButtons(targetSlider);
+  createButtons(targetSlider, i);
 }
 findTargetObj(targetSearchID);
 
 // create Click slides
 function createButtons(targetSlider) {
   targetSlider.slides.forEach(function (slide) {
+    i++;
     productSlides.insertAdjacentHTML(
       "beforeend",
-      "<button onclick='changeSlideImg()'>Slide no</button>" // set the same background for main slider
+      "<button class='slides' style='background-image: url(" +
+        targetSlider.slides[i - 1] +
+        ")' onclick='changeSlideImg(" +
+        i +
+        ")'>" +
+        i +
+        "</button>" // set the same background for main slider
     );
   });
+  //onload
+  productSlider.style.backgroundImage = "url(" + targetSlider.slides[0] + ")";
+  +");";
 }
 
 function changeSlideImg(slideID) {
-  productSlider.textContent = targetSlider[slideID - 1];
+  productSlider.style.backgroundImage =
+    "url(" + targetSlider.slides[slideID - 1];
+  +");";
 }
